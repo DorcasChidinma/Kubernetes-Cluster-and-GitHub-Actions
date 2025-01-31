bed299386c9ff04a612ac5253a020ba957438a69
DorcasChidinma <agbakwurudorcas@gmail.com>
2025-01-31 15:11:24 +0100

    Initial commit

---
+++ node.js
@@ -0,0 +1,16 @@
+// server.js
+
+'use strict';
+
+var http = require('http');
+
+var port = process.env.PORT || 1337;
+
+
+http.createServer(function (req, res) {
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    res.end('Hello from kubernetes\n');
+
+}).listen(port);
